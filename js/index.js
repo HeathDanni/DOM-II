81fb341484c52c20c94e20e7ea2c4cca862c07a3
Heather Beckman <heatherbeckman85@gmail.com>
2020-08-10 21:57:23 -0600

    added event listeners

--- js/index.js
+++ js/index.js
@@ -1,26 +1,37 @@
 // Your code goes here
 
 const header = document.querySelector('h1');
+const navLink = document.querySelectorAll('a');
+const header2 = document.querySelectorAll('h2');
+const paragraphs = document.querySelectorAll('p');
+const images = document.querySelectorAll('img');
+const headerImage = document.querySelector('.intro');
 
 header.addEventListener('mouseover', () => header.style.color = 'green');
+
 header.addEventListener('mouseout', () => header.style.color = 'black');
 
-const image = document.querySelector('img');
+window.addEventListener('scroll', () => paragraphs.forEach((para) => {
+    para.style.backgroundColor = 'black'; 
+    para.style.color = 'white'
+}));
 
-image.addEventListener('load', () => image.style.width = '400px');
+window.addEventListener('resize', () => header2.forEach((ele) => {
+    ele.style.fontSize = '50px';
+    ele.style.color = 'magenta';
+}));
 
+window.addEventListener('dblclick', () => paragraphs.forEach((para) => {
+    para.style.backgroundColor = 'silver';
+    para.style.color = 'darkorange';
+}));
 
+window.addEventListener('keydown', () => header2.forEach((ele) => ele.style.color = 'blue'));
 
+window.addEventListener('load', () => header.style.color = 'orange');
 
+window.addEventListener('wheel', () => navLink.forEach((ele) => ele.style.color = 'green'));
 
+window.addEventListener('keyup', () => paragraphs.forEach((el) => el.style.color = 'red'));
 
-// `mouseover`
-//   * `keydown`
-//   * `wheel`
-//   * `load`
-//   * `focus`
-//   * `resize`
-//   * `scroll`
-//   * `select`
-//   * `dblclick`
-//   * `drag / drop`
+window.addEventListener('keypress', () => navLink.forEach((el) => el.style.color = 'white'));
